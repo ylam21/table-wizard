414f13f9ff3dca07dc77da27be4aecb60f1fefbb
Ondrej <ondrej@dell-latitude.home>
2024-09-27 21:54:08 +0200

    more functionality

--- scripts/main.js
+++ scripts/main.js
@@ -3,14 +3,19 @@ const randNum = (max) => {
   return Math.floor(Math.random()* max + 1)
 }
 
-//  controls section
-
 // grid setup
 const rowsInput = document.getElementById("rowsInput");
 const columnsInput = document.getElementById("columnsInput");
 let rows,columns,totalItemsCalc;
 
+// datatype setup
+let checkedType;
+// arrangement setup
+let checkedArrange;
 
+// type of data
+const typeInputGetAll = document.getElementsByName("type")
+const arrangeInputGetAll = document.getElementsByName("arrange")
 
 const createLayoutBtn = document.getElementById("createLayoutBtn");
 const clearBtn = document.getElementById("clearBtn");
@@ -27,11 +32,11 @@ const clearCurrentOutput = () => {
   horizontalFlex.innerHTML = "";
   verticalFlex.innerHTML = "";
   layoutGrid.innerHTML = "";
+  sumInfo.innerHTML = "";
 };
 
 
-const adjustOutputCss = (rows,columns) => {
-  
+const adjustOutputCss = (rows,columns) => { 
   document.documentElement.style.setProperty("--rowsInput", rows);
   document.documentElement.style.setProperty("--columnsInput", columns); 
 };
@@ -61,112 +66,83 @@ const createVerticalFlex = (rows) => {
   };
 };
 
-const createContentGrid = (totalItemsCalc) => {
-  layoutGrid.classList.add("output__layout-grid");
+let sumInfo;
+sumInfo = document.getElementById("sumInfo")
 
-  for (let i = 0; i<totalItemsCalc; i++) {
-    let createNewSpan = document.createElement("span");
-  
-    createNewSpan.appendChild(document.createTextNode(i));
-    createNewSpan.classList.add("output__layout-item");
-    layoutGrid.appendChild(createNewSpan);
-  };
+const getSumInfo = () => {
+  let result;
+  let gridItems;
+  gridItems = Array.from(document.getElementsByClassName("output__layout-item"));
+  gridItems = gridItems.map(item => Number(item.innerHTML))
+  result = gridItems.reduce((acc,item)=>{
+    if (acc[item]) {
+      acc[item] = ++acc[item]
+    }
+    else {
+    acc[item] = 1
+    }
+
+  return acc
+  },{})
+
+  return result
 };
-
-const createContentGridPathOne = (totalItemsCalc) => {
-  layoutGrid.classList.add("output__layout-grid");
-
-  for (let i = 0; i<totalItemsCalc; i++) {
-   let createNewSpan = document.createElement("span");
-  
-   createNewSpan.appendChild(document.createTextNode(randNum(10)));
-   createNewSpan.classList.add("output__layout-item");
-   layoutGrid.appendChild(createNewSpan);
-  }
-}
-
-const sumOutput = () => {
- let gridItems = Array.from(document.getElementsByClassName("output__layout-item"));
-return gridItems.map(item => Number(item.innerHTML))
-}
-
-
-const defOutput = () => {
-  let array = sumOutput();
-  let output = array.reduce((acc,item)=>{
-      if (acc[item]) {
-        acc[item] = ++acc[item]
-      }
-      else {
-      acc[item] = 1
-      }
-
-    return acc
-    },{})
-
-return output
-}
-
-
-let testBtn = document.getElementById("testBtn")
-const testFunction = () => {
-// update inputs values
-rows = rowsInput.value;
-columns = columnsInput.value;
-totalItemsCalc = rows * columns;
-
-// check if input is right
-  if (rows < 0 || rows > 100 || columns < 0 || columns > 100) {
-  return 0;
-  }
-
-  else {
-  clearCurrentOutput();
-  adjustOutputCss(rows,columns);
-  createHorizontalFlex(columns);
-  createVerticalFlex(rows);
-  createContentGridPathOne(totalItemsCalc);
-
-  // sum info
-    let sumInfo = document.getElementById("sum")
-    let mytext = defOutput()
-    sumInfo.innerHTML = JSON.stringify(mytext)
-  }
-};
-
-testBtn.addEventListener("click",testFunction)
 
 const updateAllInputValues = () => {
-  // type of data
-const typeInputGetAll = document.getElementsByName("type")
-const typeOfDataNumberInput = document.getElementById("checkTypeNumber")
-const typeOfDataStringInput = document.getElementById("checkTypeString")
-const arrangeInputGetAll = document.getElementsByName("arrange")
-
   // grid setup section
   rows = rowsInput.value;
   columns = columnsInput.value;
   totalItemsCalc = rows * columns;
-  // data type section 
-  // console.log(Array.from(typeInputGetAll).filter(x => x.checked === true)[0].value)
+  // data type section
+  checkedType = Array.from(typeInputGetAll).find(x => x.checked).value;
+  // arrangement section
+  checkedArrange = Array.from(arrangeInputGetAll).find(x => x.checked).value;
 }
 
+const findPath = (checkedType,checkedArrange,totalItemsCalc) => {
+  layoutGrid.classList.add("output__layout-grid");
+
+  if (checkedType == "typeNumber") {
+    if (checkedArrange == "arrangeRandom") {
+      for (let i = 0; i<totalItemsCalc; i++) {
+      let createNewSpan = document.createElement("span");
+      createNewSpan.appendChild(document.createTextNode(randNum(10)));
+      createNewSpan.classList.add("output__layout-item");
+      layoutGrid.appendChild(createNewSpan)}
+    }
+
+    else if (checkedArrange == "arrangeOrdered") {
+      for (let i = 0; i<totalItemsCalc; i++) {
+        let createNewSpan = document.createElement("span");
+        createNewSpan.appendChild(document.createTextNode(i+1));
+        createNewSpan.classList.add("output__layout-item");
+        layoutGrid.appendChild(createNewSpan)}
+   }
+  }
+};
+
 const createLayout = () => {
-  
+  updateAllInputValues();
   // check if input is right
   if (rows > 100 || columns > 100 || rows < 0 || columns < 0) {
-    return 0;
+    return alert("Your input is not allowed.")
+  }
+
+  else if (rows == 0 || columns == 0) {
+    return 0
   }
   
   else {
   clearCurrentOutput();
-  updateAllInputValues();
   adjustOutputCss(rows,columns);
   createHorizontalFlex(columns);
   createVerticalFlex(rows);
-  createContentGrid(totalItemsCalc);
+  findPath(checkedType,checkedArrange,totalItemsCalc);
+  // sum info update
+  if (checkedArrange != "arrangeOrdered") {
+    sumInfo.innerHTML = String(JSON.stringify(getSumInfo())).replace(/[{}]/g, '')
 }
-};
+}};
 
 // !rename functions
 createLayoutBtn.addEventListener("click",createLayout);
